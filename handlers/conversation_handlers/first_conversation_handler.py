d89daa3cc299e64c337f1812e816bbaf1446fe50
Anna Liashenko <annaliashenko10@gmail.com>
2024-04-18 08:42:30 +0300

    Added InlineKeyboard

--- handlers/conversation_handlers/first_conversation_handler.py
+++ handlers/conversation_handlers/first_conversation_handler.py
@@ -1,9 +1,10 @@
-from telegram import Update
-from telegram.ext import ConversationHandler, CommandHandler, ContextTypes, MessageHandler, filters
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ConversationHandler, CommandHandler, ContextTypes, MessageHandler, filters, \
+    CallbackQueryHandler
 
 from handlers.base_handler import BaseHandler
 
-GENDER, PHOTO, AGE = range(2)
+GENDER, PHOTO, AGE = range(3)
 
 
 class FirstConversationHandler(BaseHandler):
@@ -14,6 +15,7 @@ class FirstConversationHandler(BaseHandler):
             states={
                 GENDER: [MessageHandler(filters.Regex('^(Boy|Girl)$'), cls.gender)],
                 PHOTO: [MessageHandler(filters.PHOTO, cls.photo)],
+                AGE: [CallbackQueryHandler(cls.age)],
             },
             fallbacks=[CommandHandler('exit', cls.exit)]
         )
@@ -28,25 +30,78 @@ class FirstConversationHandler(BaseHandler):
 
     @staticmethod
     async def exit(update: Update, context: ContextTypes.DEFAULT_TYPE):
-        await update.message.reply_text(f'Exit from conversation')
+        await update.message.reply_text(f'Лівнув, БОТ!')
 
         return ConversationHandler.END
 
 
     @staticmethod
     async def gender(update: Update, context: ContextTypes.DEFAULT_TYPE):
-        await update.message.reply_text(f'You are a {update.message.text}. Share your photo, please!')
+        await update.message.reply_text(f'You BAD {update.message.text}. Share your hot photo!')
 
         return PHOTO
 
     @staticmethod
     async def photo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-        await update.message.reply_text(f'Скільки тобі років???')
+        """Sends a message with three inline buttons attached."""
+        keyboard = [
+            [
+                InlineKeyboardButton("1", callback_data="1"),
+                InlineKeyboardButton("2", callback_data="2"),
+            ],
+            [
+                InlineKeyboardButton("3", callback_data="3"),
+                InlineKeyboardButton("4", callback_data="4"),
+            ],
+            [
+                InlineKeyboardButton("5", callback_data="5"),
+                InlineKeyboardButton("6", callback_data="6"),
+            ],
+            [
+                InlineKeyboardButton("7", callback_data="7"),
+                InlineKeyboardButton("8", callback_data="8"),
+            ],
+            [
+                InlineKeyboardButton("9", callback_data="9"),
+                InlineKeyboardButton("10", callback_data="10"),
+            ],
+            [
+                InlineKeyboardButton("11", callback_data="11"),
+                InlineKeyboardButton("12", callback_data="12"),
+            ],
+            [
+                InlineKeyboardButton("13", callback_data="13"),
+                InlineKeyboardButton("14", callback_data="14"),
+            ],
+            [
+                InlineKeyboardButton("15", callback_data="15"),
+                InlineKeyboardButton("16", callback_data="16"),
+            ],
+            [
+                InlineKeyboardButton("17", callback_data="17"),
+                InlineKeyboardButton("18", callback_data="18"),
+            ],
+            [
+                InlineKeyboardButton("19", callback_data="19"),
+                InlineKeyboardButton("20", callback_data="20"),
+            ],
+        ]
+
+        reply_markup = InlineKeyboardMarkup(keyboard)
+
+        await update.message.reply_text("Thank you for your photo! How old are you?)", reply_markup=reply_markup)
 
         return AGE
 
     @staticmethod
     async def age(update: Update, context: ContextTypes.DEFAULT_TYPE):
-        await update.message.reply_text(f"you are {update.message.text} year old")
+        """Parses the CallbackQuery and updates the message text."""
+        query = update.callback_query
 
-        return MessageHandler
+        # CallbackQueries need to be answered, even if no notification to the user is needed
+        # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
+        await query.answer()
+
+        await query.edit_message_text(text=f"Your age is: {query.data}")
+
+        return ConversationHandler.END
