451573f4c395986a8486a94e0e06363a3cbe12d7
Anna Liashenko <annaliashenko10@gmail.com>
2024-04-18 09:58:28 +0300

    Added normal InlineKeyboard

--- handlers/conversation_handlers/first_conversation_handler.py
+++ handlers/conversation_handlers/first_conversation_handler.py
@@ -37,71 +37,46 @@ class FirstConversationHandler(BaseHandler):
 
     @staticmethod
     async def gender(update: Update, context: ContextTypes.DEFAULT_TYPE):
-        await update.message.reply_text(f'You BAD {update.message.text}. Share your hot photo!')
+        await update.message.reply_text(f'You very BAD {update.message.text}. Share your hot photo!')
+
+        gender = update.message.text
+        context.user_data['gender'] = gender
 
         return PHOTO
 
     @staticmethod
     async def photo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-        """Sends a message with three inline buttons attached."""
-        keyboard = [
-            [
-                InlineKeyboardButton("1", callback_data="1"),
-                InlineKeyboardButton("2", callback_data="2"),
-            ],
-            [
-                InlineKeyboardButton("3", callback_data="3"),
-                InlineKeyboardButton("4", callback_data="4"),
-            ],
-            [
-                InlineKeyboardButton("5", callback_data="5"),
-                InlineKeyboardButton("6", callback_data="6"),
-            ],
-            [
-                InlineKeyboardButton("7", callback_data="7"),
-                InlineKeyboardButton("8", callback_data="8"),
-            ],
-            [
-                InlineKeyboardButton("9", callback_data="9"),
-                InlineKeyboardButton("10", callback_data="10"),
-            ],
-            [
-                InlineKeyboardButton("11", callback_data="11"),
-                InlineKeyboardButton("12", callback_data="12"),
-            ],
-            [
-                InlineKeyboardButton("13", callback_data="13"),
-                InlineKeyboardButton("14", callback_data="14"),
-            ],
-            [
-                InlineKeyboardButton("15", callback_data="15"),
-                InlineKeyboardButton("16", callback_data="16"),
-            ],
-            [
-                InlineKeyboardButton("17", callback_data="17"),
-                InlineKeyboardButton("18", callback_data="18"),
-            ],
-            [
-                InlineKeyboardButton("19", callback_data="19"),
-                InlineKeyboardButton("20", callback_data="20"),
-            ],
-        ]
+        await update.message.reply_text(f"Thank you for your photo! What's your age?")
 
+        keyboard = []
+        number = 1
+
+        for i in range(10):
+            row = []
+
+            for j in range(5):
+                row.append(InlineKeyboardButton(f"{number}", callback_data=f"{number}"))
+                number += 1
+
+            keyboard.append(row)
         reply_markup = InlineKeyboardMarkup(keyboard)
-
-        await update.message.reply_text("Thank you for your photo! How old are you?)", reply_markup=reply_markup)
+        await update.message.reply_text("Choose your age:", reply_markup=reply_markup)
 
         return AGE
+
 
     @staticmethod
     async def age(update: Update, context: ContextTypes.DEFAULT_TYPE):
         """Parses the CallbackQuery and updates the message text."""
         query = update.callback_query
 
-        # CallbackQueries need to be answered, even if no notification to the user is needed
-        # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
+
         await query.answer()
 
-        await query.edit_message_text(text=f"Your age is: {query.data}")
+        age = query.data
+
+        context.user_data['age'] = age
+
+        await query.edit_message_text(text=f"You are {context.user_data['gender']} gender. Your age is {context.user_data['age']} years old.")
 
         return ConversationHandler.END
